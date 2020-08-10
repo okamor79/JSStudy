cb7fd5c1b8c57f12b1f425e73680c6fe528f20b6
okamor79 <okamor@gmail.com>
2020-08-11 01:30:15 +0300

    Add logical model in app

--- js/script.js
+++ js/script.js
@@ -36,13 +36,24 @@ let personalMovieDB = {
 };
 
 //3
-const 
-    a = prompt('Один из последних просмотренных фильмов?', ''),
-    b = prompt('На сколько оцените его?',''),
-    c = prompt('Один из последних просмотренных фильмов?', ''),
-    d = prompt('На сколько оцените его?','');
 
-personalMovieDB.movies[a] = b;    
-personalMovieDB.movies[c] = d;
+for (let i = 1; i <= 2; i++) {
+    let a = '';
+    while (a.length <= 0)
+   {
+    a = prompt('Один из последних просмотренных фильмов?', '');
+   }
+  let b = prompt('На сколько оцените его?','');
+  personalMovieDB.movies[a] = b;    
+
+}
+
+if (personalMovieDB.count < 10) {
+    console.log('Просмотрено довольно мало фильмов');
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count <= 30) {
+    console.log('Вы классический зритель');
+} else {
+    console.log('Вы киноман');
+}
 
 console.log(personalMovieDB);
